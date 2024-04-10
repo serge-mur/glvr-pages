fd4ed0535b29d99ce7119744a560842e54777808
Serge <serge-mur@yandex.ru>
2024-04-10 16:33:26 +0300

    4/5 page, need cor history

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function (event) {
 
-
+  // local menu
   const localMenuBtn = document.querySelector('.local-menu-btn');
   const localMenuCloseBtn = document.querySelector('.local-menu-close-btn');
   const sidebar = document.querySelector('.common-page__sidebar');
@@ -14,21 +14,33 @@ document.addEventListener("DOMContentLoaded", function (event) {
     body.classList.remove('_lock');
   });
 
-
-  const mql = window.matchMedia("(max-width: 600px)");
-
-  mql.addEventListener("change", (event) => {
-    if (event.matches) {
-      // <600
-      console.log("mobile");
-    } else {
-      // >600
-      console.log("desktop");
+  // history slider
+  const breakpoint = window.matchMedia('(max-width:991px)');
+  let eventSlider;
+  const breakpointChecker = function () {
+    if (breakpoint.matches === true) {
+        if (eventSlider !== undefined) eventSlider.destroy(true, true);
+        return;
+    } else if (breakpoint.matches === false) {
+        return enableeventSlider();
     }
-  });
+  };
+  const enableeventSlider = function () {
+    eventSlider = new Swiper('.event-slider', {
+      slidesPerView: 1,
+      slidesPerView: 2,
+      spaceBetween: 40,
+      navigation: {
+        nextEl: '.event-slider__button-next',
+        prevEl: '.event-slider__button-prev',
+      }
+    });
+  };
+  breakpoint.addListener(breakpointChecker);
+  breakpointChecker();
 
-
-  const swiper = new Swiper('.logo-slider', {
+  // logo slider
+  const logoSlider = new Swiper('.logo-slider', {
     slidesPerView: 1,
     navigation: {
       nextEl: '.logo-slider__button-next',
